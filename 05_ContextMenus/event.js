74f1ac0e0bb386e63a07295af9fcf2e0e83a0dd5
motomotoyoshi <motomoto.git@gmail.com>
2019-10-18 01:37:05 +0900

    add 05, Context menus

---
+++ 05_ContextMenus/event.js
@@ -0,0 +1,23 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(() => {
+    let parent = chrome.contextMenus.create({
+        id: 'parent',
+        title: 'Choose Background Color'
+    });
+    chrome.contextMenus.create({
+        id: 'red',
+        parentId: parent,
+        title: 'Red'
+    });
+    chrome.contextMenus.create({
+        id: 'blue',
+        parentId: parent,
+        title: 'Blue'
+    });
+    chrome.contextMenus.create({
+        id: 'green',
+        parentId: parent,
+        title: 'Green'
+    });
+})
