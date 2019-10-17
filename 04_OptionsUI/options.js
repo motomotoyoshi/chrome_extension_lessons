aec06b81bc850dd0e76674501f1dd202372a8f91
motomotoyoshi <motomoto.git@gmail.com>
2019-10-18 01:00:26 +0900

    update 04 options.js, Use chrome storage

--- 04_OptionsUI/options.js
+++ 04_OptionsUI/options.js
@@ -1,5 +1,13 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.sync.get({
+        color: 'red'
+    }, item => {
+        document.getElementById('colors').value = item.color;
+    });
+});
+
 document.getElementById('save').addEventListener('click', () => {
     let color = document.getElementById('colors').value;
     chrome.storage.sync.set({
